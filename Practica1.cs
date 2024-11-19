430e7262a90bf93df47721e2a84e17602769584b
eg0r112 <Vedromakaron@gmail.com>
2024-11-19 19:17:55 +0300

    Practica1

---
+++ Practica1.cs
@@ -0,0 +1,16 @@
+namespace Practica1;
+
+public class Program
+{
+    public static int a = 10;
+    public static double b = 10.6;
+    public static string name = "chelik";
+    public static bool yes = true;
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(name);
+        Console.WriteLine(yes);
+    }
+}
